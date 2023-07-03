9ec852d60397001b74406390750e728a7906ca04
Carlos Vinicios <vinicios_martins2011@hotmail.com>
2023-07-03 00:35:15 -0300

    Adição de mais testes para cobertura de funções

--- src/__tests__/mainWindow.test.jsx
+++ src/__tests__/mainWindow.test.jsx
@@ -2,15 +2,15 @@ import { render, screen, act, waitFor } from '@testing-library/react'
 import { MainWindow } from '../pages/mainWindow';
 import { BrowserRouter } from 'react-router-dom';
 import userEvent from '@testing-library/user-event';
+import { readCSV } from "danfojs"
 
 test("Main Window renders successfully", async () => {
     const mockedLoadDatabase = jest.fn(() => Promise.resolve([]));
-
     const originalWindow = { ...window };
     window.electron = {
         "loadDatabases": mockedLoadDatabase
     };
-    
+
     await act( async () => render(<MainWindow/>));
     
     const titleElement = screen.getByText(/Sistema de Análise de Curvas/i);
@@ -33,6 +33,9 @@ test("Main Window renders with database successfully", async () => {
     ]));
 
     const originalWindow = { ...window };
+    window.URL = {
+        createObjectURL: jest.fn()
+    };
     window.electron = {
         "loadDatabases": mockedLoadDatabase
     };
@@ -52,17 +55,29 @@ test("Main Window renders with database successfully", async () => {
     window = originalWindow;
 })
 
+jest.mock('danfojs', () => ({
+    readCSV: jest.fn()
+}));
+
 test("Main Window load database clicked successfully", async () => {
     const mockedLoadDatabase = jest.fn(() => Promise.resolve([]));
+    const mockReadDatabase = jest.fn(() => Promise.resolve("t1,t2,t3\nt1,t2,t3\n"));
     const mockedOpenDialog = jest.fn(() => Promise.resolve({
                                                     canceled: false, 
                                                     filePaths: ["/home/teste.csv"]
                                                 }));
-
+                                                
+    readCSV.mockReturnValue(Promise.resolve({
+        columns: ["t1", "t2", "t3"], 
+        values: ['t1', 't2', 't3'],
+        dtypes: ['int32', 'int32', 'int32']
+    }));
+                                                
     const originalWindow = { ...window };
     window.electron = {
         "loadDatabases": mockedLoadDatabase,
-        "openDialog": mockedOpenDialog
+        "openDialog": mockedOpenDialog,
+        "readDatabase": mockReadDatabase
     };
     
     await act( async () => {
@@ -76,5 +91,32 @@ test("Main Window load database clicked successfully", async () => {
 
     expect(mockedOpenDialog).toHaveBeenCalled();
     
+    window = originalWindow;
+})
+
+test("Main Window delete database clicked successfully", async () => {
+    const mockedLoadDatabase = jest.fn(() => Promise.resolve([
+        "WELL-00001_20170201020207.csv"
+    ]));
+
+    const originalWindow = { ...window };
+    window.electron = {
+        "loadDatabases": mockedLoadDatabase
+    };
+    
+    await act( async () => render(
+        <BrowserRouter>
+            <MainWindow/>
+        </BrowserRouter>
+    ));
+
+    await act( async () => {
+        const openDialog = await waitFor(() => screen.getByLabelText(/delete/i));
+        await userEvent.click(openDialog)
+    })
+
+    const deleteMessage = screen.getByText(/Vocé tem certeza que deseja remover esta base de dados?/i);
+    expect(deleteMessage).toBeInTheDocument();
+
     window = originalWindow;
 })
